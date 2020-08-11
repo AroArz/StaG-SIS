ddbef3ebed9e5e6382e52adf19ccb484a92cd34f
Aron Arzoomand <51719360+AroArz@users.noreply.github.com>
2020-08-11 18:17:25 +0200

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 if [ ! -d "output_dir" ]; then
-	echo "creating output_dir"
-	mkdir output_dir
+        echo "creating output_dir"
+        mkdir output_dir
 else
-	[ "$(ls -A output_dir)" ] && echo "output_dir not empty, proceeding" || echo "output_dir empty, proceeding"
-	
+        [ "$(ls -A output_dir)" ] && echo "output_dir not empty, proceeding" || echo "output_dir empty, proceeding"
+
 fi
 
 
 if [ ! -d "workdir" ]; then
-	echo "creating workdir"
-	mkdir workdir
+        echo "creating workdir"
+        mkdir workdir
 fi
 
 
 if [ "$(ls -A workdir)" ]; then
-	echo "workdir is not empty, terminating workflow"
-	exit 1
+        echo "workdir is not empty, terminating workflow"
+        exit 1
 else
-	echo "workdir is empty, proceeding"
+        echo "workdir is empty, proceeding"
 fi
 
 
@@ -29,12 +29,14 @@ Output=output_dir
 Workdir=workdir
 Database=/db
 
+export SINGULARITY_CACHEDIR=$Output
 
 ### Image execution
-sudo \
+#sudo \
 /usr/bin/singularity \
 run \
 -B $PWD,$Input:/input,$Output:/output_dir,$Workdir:/workdir,$Database:/db \
 stag.sif \
 $@
+
 
